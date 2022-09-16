07286293398cf483e52fc16c18ef6a784556a75c
Stanislav Matkov <st.matkov@yandex.ru>
2022-09-16 21:13:42 +0300

    Improvements + patch

--- src/model_specific.py
+++ src/model_specific.py
@@ -3,10 +3,10 @@ import numpy as np
 
 # -------------------- Model parameters --------------------
 
-# NSAMPLES. The number of samples used in the condensation
+# NPARTICLES. The number of particles used in the condensation
 # algorithm/particle filter.  Increasing this will likely improve the
 # results, but make the tracker slower.
-NSAMPLES = 600
+NPARTICLES = 400
 
 # CONDENSSIG. The standard deviation of the observation likelihood.
 CONDENSSIG = 0.75
@@ -14,7 +14,7 @@ CONDENSSIG = 0.75
 # FORGETTING. The forgetting factor, as described in the original paper. When
 # doing the incremental update, 1 means remember all past data, and 0
 # means remeber none of it.
-FORGETTING = 0.99
+FORGETTING = 0.95
 
 # BATCHSIZE. How often to update the eigenbasis. We've used this
 # value (update every 5th frame) fairly consistently, so it most
@@ -23,21 +23,16 @@ FORGETTING = 0.99
 # but also a little more prone to drift, and require more computation.
 BATCHSIZE = 5
 
-# DOF. Degrees of freedom of object state. Its size must coincidence with AFFSIG size. Min value is 4, max value is 6.
-# This represents [x translation, y translation, scale, aspect ratio, rotation angle, skew] dofs.
-DOF = 6
-
-# AFFSIG. These are the standard deviations of
-# the dynamics distribution, that is how much we expect the target
-# object might move from one frame to the next.  The meaning of each
-# number is as follows:
+# AFFSIG. These are the standard deviations of the dynamics distribution, 
+# that is how much we expect the target object might move from one frame to the next. 
+# The meaning of each number is as follows:
 #    AFFSIG(0) = x translation (pixels)
 #    AFFSIG(1) = y translation (pixels)
 #    AFFSIG(2) = scale
 #    AFFSIG(3) = aspect ratio
 #    AFFSIG(4) = rotation angle (radians)
 #    AFFSIG(5) = skew angle (radians)
-AFFSIG = np.array([5, 5, .01, .002, .02, .001])
+AFFSIG = np.array([10, 10, .05, .002], dtype=np.float32)
 
 # TMPLSIZE. The resolution at which the tracking window is
 # sampled, 32-by-32 pixels by default.  If your initial
@@ -49,16 +44,18 @@ TMPLSIZE = 32
 MAXBASIS = 16
 
 # RESIZE_RATE. Of input frames.
-RESIZE_RATE = 1.0
+RESIZE_RATE = 0.4
 
 # INITIAL_BOX. Location of the target object on the first frame.
 # None if not neccessary, ndarray otherwise
-INITIAL_BOX = np.array([160, 106, 62, 78, -0.02])
+#INITIAL_BOX = np.array([0.802083333, 0.113333334, 0.09375, 0.166666667, -0.02], dtype=np.float32)
+#INITIAL_BOX = np.array([0.734375, 0.5659722222222222, 0.140625, 0.22916666666666666, -0.02], dtype=np.float32)
+INITIAL_BOX = None
 
 # My custom overriden parameters used here for convenience
 # INITIAL_BOX = None
 # TMPLSIZE = 32
-# NSAMPLES = 100
+# NPARTICLES = 100
 # MAXBASIS = 16
 # BATCHSIZE = 5
 # CONDENSSIG = 0.75
@@ -71,7 +68,7 @@ INITIAL_BOX = np.array([160, 106, 62, 78, -0.02])
 # -------------------- Model dynamics --------------------
 
 def BrownianMotion(X, Noise):
-    return np.random.normal(X, Noise)
+    return np.random.normal(X, Noise) # NOTE: force float64
 
 # The dynamical process represents change of state between frames.
 # Emplace new logic here if needed
